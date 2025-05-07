e6400985f7a30f0fa73baf3b212bf25def834b12
dareunk <happyeunnn11@gmail.com>
2025-05-07 11:33:48 +0900

    initial commit

---
+++ src/test/java/com/example/itcen/ItcenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itcen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
